cd01be6240f949593f4203353ee3f6a80c1e9192
KALI-UM <KALI-UM@users.noreply.github.com>
2026-09-03 03:57:00 +0000

    deploy: KALI-UM/vars2lines-blog@8958dfb1419a5123b6579dc1655669c6c12b1cc4

---
+++ postscript-bd76a8ee.js
@@ -0,0 +1,9 @@
+await Promise.all([
+  import("./static/scripts/script-0-ec880d15.js"),
+  import("./static/scripts/script-1-f7c52b6a.js"),
+  import("./static/scripts/script-2-0e41d61b.js"),
+  import("./static/scripts/script-3-632b2822.js"),
+  import("./static/scripts/script-4-296b30c3.js"),
+  import("./static/scripts/script-5-be15df42.js")
+]);
+await import("./static/scripts/script-6-d93af9ba.js");
